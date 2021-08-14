ac36d2e39cf28b9fd2eb2bc1280eae0ac744d2c7
CrypticHushane <oshanewilliams99@outlook.com>
2021-08-13 22:34:52 -0500

    making screens ui

--- src/components/home/articles/article/index.js
+++ src/components/home/articles/article/index.js
@@ -1,11 +1,25 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Button, ScrollView, ActivityIndicator } from 'react-native'
+import { Image } from 'react-native-elements'
 
-const Article = () => {
+const Article = ({ navigation, route}) => {
+    const article = route.params
+    // console.log(navigation)
     return (
-        <View>
-            <Text>Single Article</Text>
-        </View>
+        <ScrollView>
+            <View>
+                <Image
+                    source={{uri:'https://picsum.photos/200/300'}}
+                    style={{ width: '100%', height: 200}}
+                    PlaceholderContent={<ActivityIndicator />}
+                />
+                <Button title="Go Back"onPress={() => navigation.goBack()}/>
+                <Text>{article.postData.title}</Text>
+                <Text>{article.postData.content}</Text>
+            </View>
+        </ScrollView>
+        
+        
     )
 }
 
--- src/routes/stacks.js
+++ src/routes/stacks.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createDrawerNavigator } from '@react-navigation/drawer';
 
-import Home from '../components/home/Home'
+import Home from '../components/home/articles'
 import SingleArticle from '../components/home/articles/article'
-
-// import Login from '../components/auth/Login'
 
 import VideosScreen from '../components/home/videos'
 import SingleVideo from '../components/home/videos/video'
-
-import ArticleScreen from '../components/home/articles'
 
 export const Stack = createNativeStackNavigator();
 
 export const HomeStack = () => {
     return (
-       <Stack.Navigator initialRouteName='Home_Screen'>
-        <Stack.Screen name="Home_Screen" component={Home} />
-        <Stack.Screen name="Article_Screen" component={SingleArticle} />
+       <Stack.Navigator 
+            initialRouteName='Home_Screen'
+            screenOptions={{
+                headerTitleAlign: 'center',
+                headerTintColor: 'red'
+            }}
+       
+       >
+        <Stack.Screen name="Home_Screen" component={Home} options={{headerShown: false}}/>
+        <Stack.Screen name="Article_Screen" component={SingleArticle} options={{headerShown: false}}/>
     </Stack.Navigator> 
     )
     
@@ -29,8 +29,8 @@ export const HomeStack = () => {
 export const VideoStack = () => {
     return (
       <Stack.Navigator initialRouteName='Videos_Screen'>
-        <Stack.Screen name="Videos_Screen" component={VideosScreen} />
-        <Stack.Screen name="Single_Video_Screen" component={SingleVideo} />
+        <Stack.Screen name="Videos_Screen" component={VideosScreen} options={{headerShown: false}}/>
+        <Stack.Screen name="Single_Video_Screen" component={SingleVideo} options={{headerShown: false}}/>
     </Stack.Navigator>  
     )
     
