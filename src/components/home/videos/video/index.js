f06f1ca488751b9da3b0330e30a52c1d136bab7f
CrypticHushane <oshanewilliams99@outlook.com>
2021-08-14 22:52:57 -0500

    added content to videos screen

--- src/components/home/videos/video/index.js
+++ src/components/home/videos/video/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Button, StyleSheet, Text, View } from 'react-native'
 
-const SingleVideo = () => {
+const SingleVideo = ({ navigation }) => {
     return (
         <View>
             <Text>Single Video</Text>
-            
+            <Button title="Click Me"onPress={() => navigation.navigate('Videos_Screen')}/>
         </View>
     )
 }
